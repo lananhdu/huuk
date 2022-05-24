bd44296c64213ffa958cfa45ddb56747ba600ee3
lananhdu <32170415+lananhdu@users.noreply.github.com>
2022-05-24 14:12:23 +0700

    add modal box js for team-section

--- main.js
+++ main.js
@@ -1,31 +1,30 @@
-// Get the modal
-var modal = document.getElementById("myModal");
+
 
 // Get the button that opens the modal
 var btn = document.querySelectorAll(".team-section .outer-box a");
+
 Array.from(btn).forEach(a => {
     a.addEventListener('click', function (event) {
-
         event.preventDefault();
 
+        // Get ID of the modal content stored in the href attribute
+        var modalContentId = a.getAttribute('href');
+        modalContentId = modalContentId.replace('#', '');
+
+        console.log(modalContentId);
+
+        var modal = document.getElementById(modalContentId);
+
+
+        // Show the modal on click
+        modal.style.display = "block";
+
+        var span = modal.getElementsByClassName("close")[0];
+        //When the user clicks on <span> (x), close the modal
+        span.onclick = function () {
+            modal.style.display = "none";
+
+        }
     });
 });
-// Get the <span> element that closes the modal
-var span = document.getElementsByClassName("close")[0];
 
-// When the user clicks the button, open the modal 
-btn.onclick = function () {
-    modal.style.display = "block";
-}
-
-// When the user clicks on <span> (x), close the modal
-span.onclick = function () {
-    modal.style.display = "none";
-}
-
-// When the user clicks anywhere outside of the modal, close it
-window.onclick = function (event) {
-    if (event.target == modal) {
-        modal.style.display = "none";
-    }
-}
